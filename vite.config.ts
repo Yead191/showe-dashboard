36cae97b5e7ab7633c072a197c750c101c5632fb
Yead191 <yead191@gmail.com>
2026-09-18 15:53:57 +0600

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     port: 5174,
     open: true,
-    host:'0.0.0.0'
+    host: "0.0.0.0",
   },
-})
+  preview: {
+    allowedHosts: ["showe.biz"],
+  },
+});
